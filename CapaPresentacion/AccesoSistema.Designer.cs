823113d84079876e6e0bd9562a531db7083f1d6d
GersonFlores03 <xdhaber12@gmail.com>
2024-10-25 10:39:53 -0500

    viewPersonalAndCar

--- CapaPresentacion/AccesoSistema.Designer.cs
+++ CapaPresentacion/AccesoSistema.Designer.cs
@@ -81,7 +81,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(156, 228);
+            this.label1.Location = new System.Drawing.Point(152, 224);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 24);
